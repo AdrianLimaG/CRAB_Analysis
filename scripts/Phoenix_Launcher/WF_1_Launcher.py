0b1b5d91f09b7206decc0031c3d33e10a572e520
Adrian Lima <99678095+AdrianLimaG@users.noreply.github.com>
2023-06-26 11:45:53 -0500

    small updates to ph

--- scripts/Phoenix_Launcher/WF_1_Launcher.py
+++ scripts/Phoenix_Launcher/WF_1_Launcher.py
@@ -1,10 +1,15 @@
-
+import os
 from Phoenix_Launcher.WF_1_helper import sample_organizer, run_phoniex_pipeline
 
 
 def WF_1_Launch_Phoniex(path_to_samples,sample_sheet_directory,rundate,ph_output_dir,path_to_p,kraken_p):
     #need to place phoniex in resources
     #command to run
+
+    path_to_samples=path_to_samples+"sample_fastq/"+rundate
+    if not(os.path.exists(ph_output_dir+"/Output/"+rundate)):
+        os.mkdir(ph_output_dir+"/Output/"+rundate)
+        ph_output_dir= ph_output_dir+"/Output/"+rundate
 
     p_hsn=sample_organizer(path_to_samples,sample_sheet_directory+"/"+rundate+".csv")
 
